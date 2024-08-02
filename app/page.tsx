eb6218e60f2fe69aa8ccfa1d6f0dec76840f29ca
Poxie <poxen12@hotmail.com>
2024-08-02 15:35:00 +0200

    Cleanup nexts starter styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 
 export default function Home() {
   const t = useTranslations('home');
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>
         {t('title')}
       </h1>
