a7ebd424ba10da03d16bc434faeb4ea9cf945756
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-08-06 15:46:56 +0200

    add scrollToSection util to scroll to specific section id on page

---
+++ utils/scrollToSection.ts
@@ -0,0 +1,14 @@
+const OFFSET = 0;
+const SMALL_OFFSET = 30;
+export default function scrollToSection(sectionId: string) {
+  const windowSize = window.innerWidth;
+  const isSmallScreen = windowSize < 768;
+
+  const section = document.getElementById(sectionId);
+  if (section) {
+    window.scrollTo({
+      top: section.offsetTop - (isSmallScreen ? SMALL_OFFSET : OFFSET),
+      behavior: 'smooth',
+    });
+  }
+}
