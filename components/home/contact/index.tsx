f3ed34e61e873f85694c5586c621d94f08748579
Poxie <poxen12@hotmail.com>
2024-08-06 22:24:52 +0200

    Prevent apostrophe from failing build

--- components/home/contact/index.tsx
+++ components/home/contact/index.tsx
@@ -3,8 +3,8 @@ import ContactForm from "./ContactForm";
 export default function Contact() {
     return(
         <section className="main-width py-28">
-            <h2 className="mb-12 text-5xl text-center">
-                Let's get in touch
+            <h2 className="mb-8 text-5xl text-center">
+                {`Let's get in touch`}
             </h2>
             <ContactForm />
         </section>
