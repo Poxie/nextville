ee3ab82d810b15a4a9e610895bd5ffc6de16843c
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-08-06 15:43:31 +0200

    add button component

---
+++ components/ui/button/index.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from "tailwind-merge";
+
+export default function Button({ children, className, onClick, disabled, type='primary' }: {
+    children: React.ReactNode;
+    className?: string;
+    onClick?: () => void;
+    disabled?: boolean;
+    type?: 'primary' | 'secondary';
+}) {
+    className = twMerge(
+        'px-4 py-3 font-medium rounded-md border-[1px] transition-colors duration-300',
+        type === 'primary' && 'bg-c-primary text-primary hover:bg-c-primary/90',
+        type === 'secondary' && 'text-light border-dark hover:bg-dark hover:text-light',
+        className,
+    )
+
+    return(
+        <button
+            className={className}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    )
+}
