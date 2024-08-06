1a00116cdfc77aeb8cc6eba0d1d5e0c83ecf258c
Poxie <poxen12@hotmail.com>
2024-08-06 22:34:20 +0200

    Change contact label & add id to contact section

--- components/home/contact/index.tsx
+++ components/home/contact/index.tsx
@@ -2,7 +2,7 @@ import ContactForm from "./ContactForm";
 
 export default function Contact() {
     return(
-        <section className="main-width py-28">
+        <section className="main-width py-28" id="contact">
             <h2 className="mb-8 text-5xl text-center">
                 {`Let's get in touch`}
             </h2>
