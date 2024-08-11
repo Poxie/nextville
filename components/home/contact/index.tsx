fd26a936f4ff15795f90fa8bd852bc2499c141c6
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-08-11 10:51:33 +0200

    add gradient bg to contact section

--- components/home/contact/index.tsx
+++ components/home/contact/index.tsx
@@ -2,11 +2,14 @@ import ContactForm from "./ContactForm";
 
 export default function Contact() {
     return(
-        <section className="main-width py-28" id="contact">
+        <div className=' from-white via-white to-[#FCC383] from-60% w-full'>
+            <section className="px-[22.5px] py-28 bg-gradient-to-b" id="contact">
             <h2 className="mb-8 text-5xl text-center">
                 {`Let's get in touch`}
             </h2>
             <ContactForm />
         </section>
+        </div>
+      
     )
 }
