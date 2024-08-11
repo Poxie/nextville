a359adc0c835f70392aefdd0a2499f8e5a398e04
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-08-11 10:51:09 +0200

    alter project images

--- components/home/investments/InvestmentProject.tsx
+++ components/home/investments/InvestmentProject.tsx
@@ -20,7 +20,7 @@ export default function InvestmentProject({ project, className }: {
                 height={300}
                 src={`/${project.image}`}
                 alt={project.title}
-                className="w-full group-hover:scale-105 transition-transform duration-500"
+                className="w-full h-full object-fit group-hover:scale-105 transition-transform duration-500"
             />
             <div className="p-4 bg-dark-gradient absolute bottom-0 w-full left-0 md:opacity-0 md:translate-y-16 group-hover:translate-y-0 group-hover:opacity-100 transition-[transform,opacity] duration-500 ease-in-out">
                 <span className="text-xl">
