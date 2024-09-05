56b8a18fc476fc247f655ad9e54d116b6647d898
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-09-05 19:17:42 +0200

    add spacing to contact form

--- components/home/contact/index.tsx
+++ components/home/contact/index.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 export default function Contact() {
     return(
         <div className=' from-white via-white to-[#FCC383] from-60% w-full'>
-            <section className="px-[22.5px] py-28 bg-gradient-to-b" id="contact">
+            <section className="px-[22.5px] pt-28 pb-48 bg-gradient-to-b" id="contact">
             <h2 className="mb-8 text-5xl text-center">
                 {`Let's get in touch`}
             </h2>
