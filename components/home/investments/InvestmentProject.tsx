6ea3293174c8777ab49da2e22747a441642228f1
Albin Kärvling <70824199+Poxie@users.noreply.github.com>
2024-09-05 17:56:18 +0200

    Update InvestmentProject.tsx

--- components/home/investments/InvestmentProject.tsx
+++ components/home/investments/InvestmentProject.tsx
@@ -34,27 +34,6 @@ export default function InvestmentProject({ project, className }: {
         </>
     )
     return(
-<<<<<<< HEAD
-        <a 
-            target="_blank"
-            href={project.href}
-            className={twMerge(
-                "group relative flex-1 rounded-md overflow-hidden",
-                className,
-            )}
-        >
-            <Image 
-                width={500}
-                height={300}
-                src={`/${project.image}`}
-                alt={project.title}
-                className="w-full h-full object-fit group-hover:scale-105 transition-transform duration-500"
-            />
-            <div className="p-4 bg-dark-gradient absolute bottom-0 w-full left-0 md:opacity-0 md:translate-y-16 group-hover:translate-y-0 group-hover:opacity-100 transition-[transform,opacity] duration-500 ease-in-out">
-                <span className="text-xl">
-                    {project.title}
-                </span>
-=======
         project.href ? (
             <a
                 target="_blank"
@@ -66,7 +45,6 @@ export default function InvestmentProject({ project, className }: {
         ) : (
             <div {...props}>
                 {children}
->>>>>>> 9a01f7c (Add rest of investments)
             </div>
         )
     )
