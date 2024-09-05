2eb0d49fb03e00d7939e301ab89b6cd792d944a6
Berget1411 <107999361+Berget1411@users.noreply.github.com>
2024-09-05 18:46:55 +0200

    loooool

--- components/home/values/Value.tsx
+++ components/home/values/Value.tsx
@@ -1,14 +1,24 @@
-import { twMerge } from "tailwind-merge";
 
-export default function Value({ children, className }: {
+import { twMerge } from "tailwind-merge";
+import useAnimateIntoView, { DEFAULT_INITIAL_STATE } from '@/hooks/useAnimateIntoView';
+import { useRef } from 'react';
+
+export default function Value({ children, className, index, siblingRef }: {
     children: React.ReactNode;
     className?: string;
+    index: number;
+    siblingRef?: React.RefObject<HTMLElement>;
 }) {
+    const ref = useRef<HTMLDivElement>(null);
+    useAnimateIntoView(ref, { delay: 200 + index * 200, siblingRef });
     return(
         <div className={twMerge(
             "p-4 text-lg",
             className,
-        )}>
+        )} 
+        ref={ref}
+        style={DEFAULT_INITIAL_STATE}
+        >
             {children}
         </div>
     )
